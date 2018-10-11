- Fix up AggregateAgent and Network init() and solve(). update some inf tests.   - Replacing DeviceSetAgent with DeviceAgent(DeviceSet(...) has caused a change in precision error *somehow*.       Can't figure it out! Had to change DeviceSet.solve() default to 5e-6 from 1e-6 to not get +ve-d-l-s error with Bloody TDevice.       All tests pass, commiting will come back to all this if necessary.

basedevice.py
@@ -115,7 +115,7 @@ class BaseDevice(ABC):
   def to_dict(self):
     pass
 
-  def step(self, s, p, stepsize, solver_options={}):
+  def step(self, p, s, stepsize, solver_options={}):
     ''' Take one step towards optimal demand for price vector `p`, using stepsize plus limited
     minimization. This means stepsize sets the upper bound of change in x, but stepsize can be large
     since limited minimization limits severity of overshoots. This method does not modify the agent.
@@ -162,10 +162,9 @@ class BaseDevice(ABC):
     @see http://www.pyopt.org/reference/optimizers.slsqp.html
     @see https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
     '''
-    _solver_options = {'ftol': 1e-6, 'maxiter': 500}
+    _solver_options = {'ftol': 5e-6, 'maxiter': 500}
     _solver_options.update(solver_options)
-    logger.debug(_solver_options)
-
+    # logger.info(_solver_options)
     s0 = s0 if s0 is not None else self.project(np.zeros(self.shape))
 
     if (self.bounds[:, 0] == self.bounds[:, 1]).all():